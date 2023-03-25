daaf0cb13f20eb3458500c11cf6ad3d72a1d997f
jonahsebright <42495766+jonahsebright@users.noreply.github.com>
2023-03-25 18:06:10 +0100

    add todo

--- src/test/java/ch/progradler/rat_um_rad/client/command_line/UsernameHandlerTest.java
+++ src/test/java/ch/progradler/rat_um_rad/client/command_line/UsernameHandlerTest.java
@@ -1,16 +1,13 @@
 package ch.progradler.rat_um_rad.client.command_line;
 
 import ch.progradler.rat_um_rad.client.utils.ComputerInfo;
-import ch.progradler.rat_um_rad.server.gateway.OutputPacketGateway;
-import ch.progradler.rat_um_rad.server.repositories.IUserRepository;
-import ch.progradler.rat_um_rad.server.services.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -59,7 +56,7 @@ class UsernameHandlerTest {
     }
 
     @Test
-    void chooseAndSendUsername() { //TODO
+    void chooseAndSendUsername() { //TODO see handleMessageBroadCastsThisMessageWIthCorrectUsernameToAllClientsExceptThatUser() in UserService
     }
 
     @Test
