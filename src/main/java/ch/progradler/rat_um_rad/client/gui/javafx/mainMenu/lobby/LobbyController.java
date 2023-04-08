8010bee105fa582c3642be6988cdb50b95da3d34
yaowen rui <yaowen.rui@stud.unibas.ch>
2023-04-08 16:48:52 +0200

    fxml lobby view

--- src/main/java/ch/progradler/rat_um_rad/client/gui/javafx/mainMenu/lobby/LobbyController.java
+++ src/main/java/ch/progradler/rat_um_rad/client/gui/javafx/mainMenu/lobby/LobbyController.java
@@ -1,21 +1,51 @@
 package ch.progradler.rat_um_rad.client.gui.javafx.mainMenu.lobby;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LobbyController implements Initializable {
+    public ListView<String> openGamesListView;
+    public Button showAllGamesButton;
+    public Button leaveLobbyButton;
+    public Button createGameButton;
+    public TextArea currentPlayersTextArea;
+    public TextField gameIdTextField;
+    public Button joinButton;
+
     private LobbyModel lobbyModel;
-    @FXML
-    private ListView<String> listView;
+
+    //private ListView<String> listView;
+
     public LobbyController() {
         this.lobbyModel = new LobbyModel();
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        listView.getItems().addAll(this.lobbyModel.getGameNames());
+        openGamesListView.getItems().addAll(this.lobbyModel.getGameNames());
+    }
+
+    @FXML
+    public void showAllGamesAction(ActionEvent event){
+
+    }
+
+    @FXML
+    public void leaveLobbyAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void createGameAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void joinAction(ActionEvent actionEvent) {
     }
 }
