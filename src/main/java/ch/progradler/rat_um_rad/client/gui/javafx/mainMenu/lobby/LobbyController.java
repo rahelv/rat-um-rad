f9b567d18ce2afa47f72873aa95173d27abdfabe
Rahel <rahel.kempf@unibas.ch>
2023-04-09 17:33:27 +0200

    lobbycontroller eventhandlers

--- src/main/java/ch/progradler/rat_um_rad/client/gui/javafx/mainMenu/lobby/LobbyController.java
+++ src/main/java/ch/progradler/rat_um_rad/client/gui/javafx/mainMenu/lobby/LobbyController.java
@@ -1,38 +1,39 @@
 package ch.progradler.rat_um_rad.client.gui.javafx.mainMenu.lobby;
 
+import ch.progradler.rat_um_rad.client.services.GameService;
+import ch.progradler.rat_um_rad.client.services.IGameService;
+import ch.progradler.rat_um_rad.server.models.Game;
+import ch.progradler.rat_um_rad.shared.models.game.GameBase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LobbyController implements Initializable {
-    public ListView<String> openGamesListView;
-    public Button showAllGamesButton;
-    public Button leaveLobbyButton;
-    public Button createGameButton;
+    private IGameService gameService;
+    private LobbyModel lobbyModel;
+    public ListView<GameBase> openGamesListView;
     public TextArea currentPlayersTextArea;
     public TextField gameIdTextField;
-    public Button joinButton;
-
-    private LobbyModel lobbyModel;
-
-    //private ListView<String> listView;
-
-    public LobbyController() {
-        this.lobbyModel = new LobbyModel();
-    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //this.openGamesListView.getItems().addAll(this.lobbyModel.getGameNamesList());
-        this.openGamesListView.setItems(this.lobbyModel.getGameNamesList());
+        this.gameService = new GameService();
+        this.lobbyModel = new LobbyModel();
+        try {
+            this.gameService.requestWaitingGames();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.openGamesListView.setItems(this.lobbyModel.getGameInfoList());
         openGamesListView.setCellFactory(param -> new Cell());
         //each item of listView should have 2 buttons:list players and join game
     }
-
 
     @FXML
     public void showAllGamesAction(ActionEvent event){
@@ -41,37 +42,52 @@ public class LobbyController implements Initializable {
 
     @FXML
     public void leaveLobbyAction(ActionEvent actionEvent) {
+        System.out.println("leave lobby");
     }
 
     @FXML
     public void createGameAction(ActionEvent actionEvent) {
+        //TODO: create Game dialog
+        System.out.println("create game");
+        try {
+            this.gameService.createGame(5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
-    public void joinAction(ActionEvent actionEvent) {
+    public void joinGameAction(ActionEvent actionEvent) {
+        //TODO: get gameId (bind to textfield wo id eingegeben wurde)
+        System.out.println("joined game ");
+        //        //TODO: Anfrage an Server um Game zu jonen
     }
-    static class Cell extends ListCell<String> { //to be continued,nicht so sicher
+
+    static class Cell extends ListCell<GameBase> {
         HBox hbox = new HBox();
         Label nameLabel = new Label();
-        Button listPlayers = new Button("list");
-        Button enterGame = new Button("join");
+        Button listPlayersButton = new Button("players");
+        Button enterGameButton = new Button("join");
         public Cell() {
             super();
-            hbox.getChildren().addAll(listPlayers,enterGame);
-            //add button actions
+            hbox.getChildren().addAll(nameLabel, listPlayersButton, enterGameButton);
+            enterGameButton.setOnAction(event -> {
+                System.out.println("wanting to join game " + getItem().getId());
+                //TODO: send anfrage to server: OutputPacketGatewaySingleton.;
+            });
+            listPlayersButton.setOnAction(event -> {
+                System.out.println("listing all players in this game");
+            });
         }
-        @Override
-        protected void updateItem(String name,boolean empty){
-            super.updateItem(name,empty);
+        protected void updateItem(GameBase item, boolean empty){
+            super.updateItem(item, empty);
             setText(null);
             setGraphic(null);
-            if(name != null && !empty){
-                nameLabel.setText(name);
+            if(item != null && !empty){
+                nameLabel.setText(item.getId());
                 setGraphic(hbox);
             }
         }
-    }
-    public void listPlayersInGame(){
 
     }
 }
--- src/main/java/ch/progradler/rat_um_rad/client/gui/javafx/mainMenu/lobby/LobbyModel.java
+++ src/main/java/ch/progradler/rat_um_rad/client/gui/javafx/mainMenu/lobby/LobbyModel.java
@@ -1,46 +1,25 @@
 package ch.progradler.rat_um_rad.client.gui.javafx.mainMenu.lobby;
 
 
-import ch.progradler.rat_um_rad.client.models.GameInfo;
-
+import ch.progradler.rat_um_rad.shared.models.game.GameBase;
+import ch.progradler.rat_um_rad.shared.models.game.GameStatus;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListCell;
-import javafx.scene.layout.HBox;
 
 public class LobbyModel {
     //Was braucht es hier?
-    private ObservableList<GameInfo> gameInfoList; //TODO: model has too much information, new model?
+    private ObservableList<GameBase> gameInfoList; //TODO: model has too much information, new model?
     private Integer currentlyOnlinePlayers; //TODO: evt. Liste dieser Spieler
-    private ObservableList<String> gameNamesList;
-    private Integer playersInGameGroup;
     public LobbyModel() {
-        //this.gameList = new ArrayList<GameInfo>();
         this.gameInfoList = FXCollections.observableArrayList();
-        gameInfoList.add(new GameInfo("1", "open",5, "spoiel1"));
-        gameInfoList.add(new GameInfo("2", "open",4, "lalalulu"));
-        gameInfoList.add(new GameInfo("3", "open",5, "mergwönned"));
-        gameInfoList.add(new GameInfo("4", "open",3, "neimer"));
-        gameInfoList.add(new GameInfo("5", "open",4, "die coole"));
-
         this.currentlyOnlinePlayers = 12;
     }
 
-    /*
-    public List<String> getGameNames() {
-        List<String> list = new ArrayList<String>();
-        for (GameInfo game : gameList) {
-            list.add(game.getName());
-        }
-        return list;
-    }
-     */
-    public ObservableList<String> getGameNamesList(){
-        for(GameInfo game : gameInfoList){
-            gameNamesList.add(game.getName());
-        }
-        return gameNamesList;
+    public void addGameToLobby(GameBase game) {
+        this.gameInfoList.add(game);
     }
 
+    public ObservableList<GameBase> getGameInfoList() {
+        return gameInfoList;
+    }
 }
