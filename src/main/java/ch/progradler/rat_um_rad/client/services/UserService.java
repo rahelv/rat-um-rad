bc930bb23831c89188e6284e2f84a5b81b1bc75e
yaowen rui <yaowen.rui@stud.unibas.ch>
2023-04-11 19:53:41 +0200

    add whisper chat

--- src/main/java/ch/progradler/rat_um_rad/client/services/UserService.java
+++ src/main/java/ch/progradler/rat_um_rad/client/services/UserService.java
@@ -38,6 +38,11 @@ public class UserService implements IUserService {
     }
 
     @Override
+    public void sendGameInternalMessage(String message) throws IOException {
+
+    }
+
+    @Override
     public void sendWhisperMessage(String message, String toUsername) throws IOException {
         Packet packet = new Packet(Command.SEND_WHISPER_CHAT,
                 new ChatMessage(toUsername, message),
