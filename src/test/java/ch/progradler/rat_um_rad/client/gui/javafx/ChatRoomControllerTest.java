6c180c95273fce93e6f05bb73f766129b6a3465a
jonahsebright <42495766+jonahsebright@users.noreply.github.com>
2023-04-14 01:05:00 +0200

    Update ChatRoomControllerTest.java

--- src/test/java/ch/progradler/rat_um_rad/client/gui/javafx/ChatRoomControllerTest.java
+++ src/test/java/ch/progradler/rat_um_rad/client/gui/javafx/ChatRoomControllerTest.java
@@ -3,13 +3,10 @@ package ch.progradler.rat_um_rad.client.gui.javafx;
 import ch.progradler.rat_um_rad.client.gui.javafx.game.chatRoom.ChatRoomController;
 import ch.progradler.rat_um_rad.client.gui.javafx.game.chatRoom.ChatRoomModel;
 import ch.progradler.rat_um_rad.client.services.IUserService;
-import ch.progradler.rat_um_rad.client.services.UserService;
-import javafx.collections.ObservableList;
-import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,36 +16,75 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 @ExtendWith(MockitoExtension.class)
 public class ChatRoomControllerTest {
 
-    public TextField chatMsgTextField;
 
-    public ListView chatPaneListView;
+    //@Mock
+    ChoiceBox<String> chatChoiceBox;
+    //@Mock
+    TextField chatMsgTextField;
+    //@Mock
+    ListView chatPaneListView;
+
     @Mock
     IUserService userService;
     @Mock
-    private ChatRoomController chatRoomController;
-    @Mock
     ChatRoomModel chatRoomModel;
 
+    @InjectMocks
+    ChatRoomController chatRoomController;
+
     @BeforeEach
-    void setUp() throws Exception{
+    void setUp() {
         chatRoomController = new ChatRoomController();
-        userService = new UserService();
     }
+
+    @Test
+    void sendChatButtonActionSendsInputToAllIfChatChoiceBoxIsAll() throws IOException {
+        //chatChoiceBox = mock(ChoiceBox.class);
+        chatChoiceBox = new ChoiceBox<>();
+        chatChoiceBox.setValue("all");
+
+        //chatRoomController.setChatChoiceBox(chatChoiceBox);
+
+        // prepare
+        String chatInput = "userInput";
+        when(chatRoomModel.getTextInputContent()).thenReturn(chatInput);
+        //when(chatChoiceBox.getValue()).thenReturn("all");
+
+        // execute
+        chatRoomController.sendChatMessageAction(new ActionEvent());
+
+        // assert
+        verify(userService).sendBroadCastMessage(chatInput);
+    }
+
+    @Test
+    void sendChatButtonActionClearsTextField() throws IOException {
+        // execute
+        chatRoomController.sendChatMessageAction(new ActionEvent());
+
+        // assert
+        verify(chatMsgTextField).clear();
+    }
+
     @Test
     void listViewTest() {
-        chatRoomController.chatMsgTextField.setText("expected chat message");
-        chatRoomController.sendButton.setOnAction(event -> {
-            try {
-                userService.sendBroadCastMessage("expected chat message");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+         /*String chatInput ="userInput";
+        when(chatRoomModel.getTextInputContent()).thenReturn(chatInput);
+        chatRoomController.sendChatMessageAction(new ActionEvent());
+
+
+        userService.sendBroadCastMessage(chatInput);
+
         ObservableList items = chatRoomController.chatPaneListView.getItems();
         Assertions.assertEquals("expected chat message",items.toString());
+
+          */
     }
 }
