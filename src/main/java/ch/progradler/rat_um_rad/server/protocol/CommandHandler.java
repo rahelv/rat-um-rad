fc83b59316bbed646ad228d61255f59221824879
jonahsebright <42495766+jonahsebright@users.noreply.github.com>
2023-04-19 14:21:18 +0200

    Update CommandHandler.java

--- src/main/java/ch/progradler/rat_um_rad/server/protocol/CommandHandler.java
+++ src/main/java/ch/progradler/rat_um_rad/server/protocol/CommandHandler.java
@@ -40,7 +40,7 @@ public class CommandHandler implements InputPacketGateway {
     public void handleCommand(Packet<ClientCommand> packet, String ipAddress) {
         // TODO: unittest
 
-        LOGGER.info("Received client command " + packet.getCommand());
+        LOGGER.info("Received client command " + packet.getCommand() + " from " + ipAddress);
 
         switch (packet.getCommand()) {
             case SEND_WHISPER_CHAT -> {
