97c43f74a57e06791df167bd982a11c5bef2c79b
jonahsebright <42495766+jonahsebright@users.noreply.github.com>
2023-05-20 17:26:40 +0200

    locale database folder is created before using

--- src/main/java/ch/progradler/rat_um_rad/shared/database/FileStorage.java
+++ src/main/java/ch/progradler/rat_um_rad/shared/database/FileStorage.java
@@ -1,6 +1,7 @@
 package ch.progradler.rat_um_rad.shared.database;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,19 +12,25 @@ import java.nio.file.Paths;
  * Implementation of {@link LocalStorage} using Files.
  */
 public class FileStorage implements LocalStorage {
-    private static final String LOCALE_FILES_STORAGE_FOLDER = "locale_database/";
+    private static final String LOCALE_FILES_STORAGE_FOLDER = "locale_database";
 
     @Override
     public void save(String content, String filename) throws IOException {
-        filename = LOCALE_FILES_STORAGE_FOLDER + filename;
+        ensureCreateDatabaseDir();
+        filename = LOCALE_FILES_STORAGE_FOLDER + "/" + filename;
         BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
         writer.write(content);
         writer.close();
     }
 
+    private void ensureCreateDatabaseDir() {
+        File DB_Dir = new File(LOCALE_FILES_STORAGE_FOLDER);
+        DB_Dir.mkdirs();
+    }
+
     @Override
     public String read(String filename) throws IOException {
-        filename = LOCALE_FILES_STORAGE_FOLDER + filename;
+        filename = LOCALE_FILES_STORAGE_FOLDER + "/" + filename;
         Path path = Paths.get(filename);
         return Files.readString(path);
     }
